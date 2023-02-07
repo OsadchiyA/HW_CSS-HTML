2937026e8d6626208e41e229adbbf94c15c93b80
Alex Os <feyk.takoy.feyk@yandex.ru>
2023-02-07 20:22:36 +0300

    Home work 10

---
+++ script.js
@@ -0,0 +1,8 @@
+let menuActive = document.querySelector('.dropdown_menu');
+let headerMenu = document.querySelector('.user-menu');
+
+function toggleMenu(){
+    menuActive.classList.toggle('hidden');
+}
+
+headerMenu.addEventListener('click', toggleMenu);
